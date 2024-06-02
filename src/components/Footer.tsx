a67a3cc6a0591d4b7bd43421993d1ea3fc312da4
Logan <ltao47@gatech.edu>
2024-06-02 19:00:47 +0200

    Update Footer.tsx
    Changed the original logo to rex logo which hyperlinks to the team website.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
-import { Logo } from './Logo';
 
 export function Footer() {
   return (
     <footer>
       <div className="mt-8 flex h-24 flex-col items-center justify-center p-2">
-        <Logo />
+        <a href = 'https://team1727.org/'><img src = "src\assets\Revised_White_REX_Wordmark.png" style={{ width: '600px', height: 'auto' }}/></a>
       </div>
+
     </footer>
+
   );
 }
